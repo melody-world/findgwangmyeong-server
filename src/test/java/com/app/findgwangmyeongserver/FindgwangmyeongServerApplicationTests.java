cc199b96bcdba0602a77cb319843c936b4f1a879
eeesnghyun <szhyun2002@gmail.com>
2023-07-16 15:54:19 +0900

    init

---
+++ src/test/java/com/app/findgwangmyeongserver/FindgwangmyeongServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.findgwangmyeongserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindgwangmyeongServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
